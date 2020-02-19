2e2584c2e36edf2f4062b4f74b378267f2916261
rizkykrogh <rizky.krogh@icloud.com>
2020-02-19 14:23:12 +0700

    update

---
+++ tugasKamis-week4-fase0/exercise10.js
@@ -0,0 +1,28 @@
+function changeMe(arr) {
+    // you can only write your code here!
+    for(var i = 0; i < arr.length; i++) {
+        console.log(`${i + 1}. ${arr[i][0]} ${arr[i][1]}:`);
+        var currentYear = new Date().getFullYear();
+        var result = {
+            firstName: arr[i][0],
+            lastName: arr[i][1],
+            gender: arr[i][2],
+            age: arr[i][3] === undefined ? 'Invalid Birth Year' : arr[i][3] > currentYear ? 'Invalid Birth Year' : currentYear - arr[i][3]
+        }
+        console.log(result);
+    }
+  }
+  
+  // TEST CASES
+  changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); 
+  // 1. Christ Evans:
+  // { firstName: 'Christ',
+  //   lastName: 'Evans',
+  //   gender: 'Male',
+  //   age: 37 }
+  // 2. Robert Downey:
+  // { firstName: 'Robert',
+  //   lastName: 'Downey',
+  //   gender: 'Male',
+  //   age: 'Invalid Birth Year' }
+  changeMe([]); // ""
